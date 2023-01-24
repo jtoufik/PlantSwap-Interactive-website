5c0bdf2066acc3ac29c7ef64a1eb484c010c1712
jtoufik <112856590+jtoufik@users.noreply.github.com>
2023-01-24 23:26:32 +0100

    .

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,11 @@
 
-// const articleForm = document.querySelector('#article-form')
 
-// let showOrHide = function(){
-//     articleForm.style.display = 'block'
-// }
 
 let toggleBtn = document.querySelector('#btn-toggle');
 let plantExchange = document.querySelector('#article-form');
 
 toggleBtn.addEventListener('click', function() {
-  // toggleBtn.classList.toggle('is-closed');
+
   plantExchange.style.display = 'block'
   plantExchange.classList.toggle('.slide');
 })
